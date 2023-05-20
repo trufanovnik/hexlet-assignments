3bfe42ac00c9fb9a92ff03d30fc86d38c094a052
@hexlet/cli <support@hexlet.io>
2023-05-20 19:35:56 +0300

    submit rails-real-ru/nested-forms

---
+++ rails-real-ru/nested-forms/app/models/resume.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class Resume < ApplicationRecord
+  validates :name, presence: true
+  validates :contact, presence: true
+
+  # BEGIN
+  has_many :educations, class_name: 'ResumeEducation', inverse_of: :resume, dependent: :destroy
+  has_many :works, class_name: 'ResumeWork', inverse_of: :resume, dependent: :destroy
+  accepts_nested_attributes_for :educations, :works, allow_destroy: true
+  # END
+end
